e5ff9281b073b596adfb05cbfc764e38331c6b89
Binod Bist <thebinodbist@gmail.com>
2025-12-16 10:11:40 +0545

    add categories

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -119,3 +119,11 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = ['blog_main/static']
 
+
+
+DEFAULT_AUTO_FIELD ='django.db.models.BigAutoFiled'
+
+
+MEDIA_URL='/media'
+MEDIA_ROOT= BASE_DIR /'media'
+
